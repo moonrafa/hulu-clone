8c1ff2eed34c5908a616ac6bbdc6916117ff1613
Rafaelly Rodrigues <rrafasrodrigues@gmail.com>
2022-10-25 14:09:00 -0300

    nav completed

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ import {
 const Header = () => {
   return (
     <header className="flex flex-col sm:flex-row h-auto items-center justify-between m-5">
-      <div className=" flex flex-grow justify-evenly max-w-2xl">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="home" Icon={HomeIcon} />
         <HeaderItem title="verified" Icon={BadgeCheckIcon} />
         <HeaderItem title="collections" Icon={CollectionIcon} />
@@ -21,7 +21,7 @@ const Header = () => {
         <HeaderItem title="account" Icon={UserIcon} />
       </div>
       <Image
-        className="bottom-4 object-contain cursor-pointer"
+        className="object-contain cursor-pointer"
         src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Hulu_Logo.svg"
         width={120}
         height={60}
